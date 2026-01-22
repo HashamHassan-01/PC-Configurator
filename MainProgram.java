40e03cf77968f6f7ea0df0f868f57989f638895a
HASHAM HASSAN <hashamhassan987@gmail.com>
2026-01-22 23:18:12 +0500

    Add files via upload

---
+++ MainProgram.java
@@ -0,0 +1,20 @@
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        // Set a clean system look and feel
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {}
+
+        SwingUtilities.invokeLater(() -> {
+            // Initialize Data
+            SmartPCTest.stockItems();
+
+            // Launch GUI
+            new MainGUI().setVisible(true);
+        });
+    }
+}
